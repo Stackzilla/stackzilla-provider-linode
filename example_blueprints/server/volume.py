e8bef83610b341a1a875f147da9cc2d7cc6eab72
Rob Zimmerman <rob@stackzilla.dev>
2022-11-13 19:51:46 +0000

    example updates

--- example_blueprints/server/volume.py
+++ example_blueprints/server/volume.py
@@ -1,13 +1,15 @@
 """Example volume blueprint resource for a Linode volume."""
 import os
 from stackzilla.provider.linode.volume import LinodeVolume
+from .instance import MyServer
 LinodeVolume.token = os.getenv('STACKZILLA_LINODE_TOKEN')
 
 class MyVolume(LinodeVolume):
     def __init__(self):
         super().__init__()
 
-        self.size = 120
-        self.region = 'us-west'
+        self.size = 60
+        self.region = 'us-east'
         self.tags = ['foo', 'bar', 'zim']
         self.label = 'Stackzilla_Testing_2'
+        self.instance = MyServer
